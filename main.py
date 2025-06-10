bf36f9d4365b5a79bc1cf397fcf8eb5c15ab8dc3
David Dean <runfromnuke@gmail.com>
2025-06-09 23:59:59 -0500

    feat: add verbose flag with loose checking
    Implimented weak arg-parsing.  Tech debt incurred.

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+verbose = False
 
 if len(sys.argv) == 1:
     print("No prompt entered")
     sys.exit(1)
-else:
-    prompt = sys.argv[1]
+elif len(sys.argv) >= 3:
+    if sys.argv[-1] == "--verbose":
+        verbose = True
+    else:
+        raise Exception(f"{sys.argv[-1]}: not recognized")
+
+user_prompt = sys.argv[1]
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -26,8 +32,10 @@ response = client.models.generate_content(
     contents=messages,
 )
 
+if verbose:
+    print(f"User prompt: {user_prompt}\n")
 # Print the text of the response...
 print(f"{response.text}")
-# Followed by the number of tokens consumed by the prompt and response.
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
