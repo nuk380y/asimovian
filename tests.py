b1e520e006e6490cacd8dec2ea7628a15780bf71
nuk380y <runfromnuke@gmail.com>
2025-06-10 16:16:44 -0500

    test: validate that upcoming function limits scope
    Upcoming function `get_files_info()` should _not_ allow for path
    traversal closer to root (`/`) from the passed `working_directory`
    passed to it.

---
+++ tests.py
@@ -0,0 +1,9 @@
+from functions.get_files_info import get_files_info
+
+print(f'{get_files_info("calculator", ".")}')
+
+print(f'{get_files_info("calculator", "pkg")}')
+
+print(f'{get_files_info("calculator", "/bin")}')
+
+print(f'{get_files_info("calculator", "../")}')
