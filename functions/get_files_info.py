d5be3748bdcdedde986401d939ba160ba6c6e592
nuk380y <runfromnuke@gmail.com>
2025-06-10 16:21:52 -0500

    feat: logic to parse metadata about working dir
    Created a function that is meant to provide a scope of context to then
    pass back to the AI Agent.  This can help provide cleaner and more
    pertinent responses.  Function is designed to limit context to only what
    is within the passed `working_directory`.

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    directory = os.path.join(working_directory, directory)
+
+    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
+        print(f"{os.path.abspath(working_directory)}")
+        print(f"{os.path.abspath(directory)}")
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    elif not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+    else:
+        sub_directories = os.listdir(os.path.abspath(directory))
+        output = []
+
+        for file in sub_directories:
+            file = os.path.join(directory, file)
+            filename = os.path.basename(file)
+            filesize = os.path.getsize(os.path.abspath(file))
+            is_dir = os.path.isdir(os.path.abspath(file))
+            output.append(f"- {filename}: filesize={filesize} bytes, is_dir={is_dir}")
+
+        return "\n".join(output)
