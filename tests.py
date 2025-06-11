42b9b279d6a3ca493fad2b52e390d5c8e082e933
David Dean <runfromnuke@gmail.com>
2025-06-10 21:06:43 -0500

    test: change tests for file contents
    Commented out the tests for `get_files_info`, and added initial test
    case for demoing reading and truncating a file in a specified directory.

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
+from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 
-print(f'{get_files_info("calculator", ".")}')
+## For `get_files_info`
+# print(f'{get_files_info("calculator", ".")}')
+# print(f'{get_files_info("calculator", "pkg")}')
+# print(f'{get_files_info("calculator", "/bin")}')
+# print(f'{get_files_info("calculator", "../")}')
 
-print(f'{get_files_info("calculator", "pkg")}')
-
-print(f'{get_files_info("calculator", "/bin")}')
-
-print(f'{get_files_info("calculator", "../")}')
+## For `get_file_content`
+print(f'{get_file_content("calculator", "lorem.txt")}')
