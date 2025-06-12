b1e257b87f43b5d1855988d31bf7917a5832d784
nuk380y <runfromnuke@gmail.com>
2025-06-12 15:56:46 -0500

    test: add new tests for running python scripts

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 ## For `get_files_info`
 # print(f'{get_files_info("calculator", ".")}')
@@ -17,6 +18,12 @@ from functions.write_file import write_file
 # print(f'{get_file_content("calculator", "/bin/cat")}')
 
 ## For 'write_file`
-print(f'{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}')
-print(f'{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}')
-print(f'{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}')
+# print(f'{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}')
+# print(f'{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}')
+# print(f'{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}')
+
+## For 'run_python_file'
+print(f'{run_python_file("calculator", "main.py")}')
+print(f'{run_python_file("calculator", "tests.py")}')
+print(f'{run_python_file("calculator", "../main.py")}')
+print(f'{run_python_file("calculator", "nonexistent.py")}')
